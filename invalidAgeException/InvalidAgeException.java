207cac45f79575c413c4489f4b7edcf58bd8a47d
pooja kadam <poojakadam25710@gmail.com>
2025-12-09 17:36:01 +0530

    uploaded invalid age exception code

---
+++ invalidAgeException/InvalidAgeException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package invalidAgeException;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class InvalidAgeException extends Exception{
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
